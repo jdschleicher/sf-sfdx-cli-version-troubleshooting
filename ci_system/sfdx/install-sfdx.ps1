b5b3c10f0b2d215e933415d65dc4173a1beceafc
jdschleicher <jdschleicher@gmail.com>
2023-08-17 12:04:47 -0400

    adding json verbose

--- ci_system/sfdx/install-sfdx.ps1
+++ ci_system/sfdx/install-sfdx.ps1
@@ -7,11 +7,11 @@ Write-Host "version: $cli_version"
 npm install --global $cli_version
 
 if ( $cli_version -match "sfdx" ) {
-    Write-Host " sfdx version --verbose"
-    sfdx version --verbose
+    Write-Host " sfdx version --verbose --json"
+    sfdx version --verbose --json
 } else {
-    Write-Host "sf version --verbose"
-    sf version --verbose
+    Write-Host "sf version --verbose --json"
+    sf version --verbose --json
 }
 
 
