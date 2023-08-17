634bf22f34958d06924df5e4f5ba56b429aad41a
jdschleicher <jdschleicher@gmail.com>
2023-08-17 11:17:03 -0400

    verbose troubleshooting

--- ci_system/sfdx/install-sfdx.ps1
+++ ci_system/sfdx/install-sfdx.ps1
@@ -1,14 +1,15 @@
 param($cli_version)
 
 echo "Installing SFDX CLI"
-npm install -g npm
-node --version
+
 
 npm install --global $cli_version
 
 if ( $cli_version -match "sfdx" ) {
+    Write-Host " sfdx version --verbose"
     sfdx version --verbose
 } else {
+    Write-Host "sf version --verbose"
     sf version --verbose
 }
 
