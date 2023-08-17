f82589664ff127171aa77457491c7071267abd11
jdschleicher <jdschleicher@gmail.com>
2023-08-17 11:39:38 -0400

    introducing second job for sf commands

--- ci_system/sfdx/create_scratch_org.ps1
+++ ci_system/sfdx/create_scratch_org.ps1
@@ -4,6 +4,5 @@ Write-Host "CREATING SCRATCH ORG devhub_alias: $devhub_alias"
 Write-Host "CREATING SCRATCH ORG scratch_org_alias: $scratch_org_alias"
 
 $create_scratch_result_json = $(sfdx force:org:create --targetdevhubusername $devhub_alias --definitionfile config/project-scratch-def.json --wait 15 --durationdays 1 -a $scratch_org_alias --json )
-Write-Host $create_scratch_result_json
-$create_scratch_result = $create_scratch_result_json| ConvertFrom-Json
-$create_scratch_result.result.status
+Write-Host "sfdx create scratch: $create_scratch_result_json"
+
