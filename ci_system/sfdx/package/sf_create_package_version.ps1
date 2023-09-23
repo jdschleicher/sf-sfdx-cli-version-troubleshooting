5c3129b6fc2ef63aa413ebb37d417458843dc962
jdschleicher <jdschleicher@gmail.com>
2023-09-23 16:46:06 -0400

    trying to see issue

--- ci_system/sfdx/package/sf_create_package_version.ps1
+++ ci_system/sfdx/package/sf_create_package_version.ps1
@@ -4,4 +4,4 @@ param(
 )
 
 
-sf package version create --target-dev-hub $devhub_alias --package $package_alias --code-coverage --installation-key-bypass --wait 120 --json --verbose --dev-debug --preserve
+sf package version create --target-dev-hub $devhub_alias --package $package_alias --code-coverage --installation-key-bypass --wait 120 --json --verbose -dev-debug --preserve
