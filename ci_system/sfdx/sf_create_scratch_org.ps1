d178784b9e12980fbae7a60acd8851670356acea
jdschleicher <jdschleicher@gmail.com>
2023-09-24 17:56:13 -0400

    removing nonexistent verbose flag

--- ci_system/sfdx/sf_create_scratch_org.ps1
+++ ci_system/sfdx/sf_create_scratch_org.ps1
@@ -3,8 +3,8 @@ param( $devhub_alias, $scratch_org_alias)
 Write-Host "CREATING SCRATCH ORG devhub_alias: $devhub_alias"
 Write-Host "CREATING SCRATCH ORG scratch_org_alias: $scratch_org_alias"
 
-Write-Host "sf org create scratch --target-dev-hub $devhub_alias -a $scratch_org_alias --definition-file config/project-scratch-def.json --duration-days 1 --wait 15 --json --no-namespace --verbose --dev-debug --preserve" 
-$create_scratch_result_json = $(sf org create scratch --target-dev-hub $devhub_alias -a $scratch_org_alias --definition-file config/project-scratch-def.json --duration-days 1 --wait 15 --json --no-namespace --verbose --dev-debug --preserve )
+Write-Host "sf org create scratch --target-dev-hub $devhub_alias -a $scratch_org_alias --definition-file config/project-scratch-def.json --duration-days 1 --wait 15 --json --no-namespace --dev-debug --preserve" 
+$create_scratch_result_json = $(sf org create scratch --target-dev-hub $devhub_alias -a $scratch_org_alias --definition-file config/project-scratch-def.json --duration-days 1 --wait 15 --json --no-namespace --dev-debug --preserve )
 
 Write-Host "sf create scratch: $create_scratch_result_json"
 
