5a7c1475d76bd42373401d6a77f220c89ecf0c71
jdschleicher <jdschleicher@gmail.com>
2023-09-24 10:22:00 -0400

    removing verbose commands

--- ci-cd/package/package_version_install.ps1
+++ ci-cd/package/package_version_install.ps1
@@ -4,8 +4,8 @@ Write-Host "INSTALLING PACKAGE"
 Write-Host "org alias: $org_to_install_alias"
 Write-Host "package_version_id: $package_version_id";
 
-Write-Host "sfdx force:package:install --noprompt --targetusername $org_to_install_alias --wait 20 --package $subscriber_package_version_id --upgradetype Delete --loglevel trace --json --apexcompile package --verbose --dev-debug --preserve"
-$install_package_result_json = $( sfdx force:package:install --noprompt --targetusername $org_to_install_alias --wait 20 --package $subscriber_package_version_id --upgradetype Delete --loglevel trace --json --apexcompile package --verbose --dev-debug --preserve )
+Write-Host "sfdx force:package:install --noprompt --targetusername $org_to_install_alias --wait 20 --package $subscriber_package_version_id --upgradetype Delete --loglevel trace --json --apexcompile package"
+$install_package_result_json = $( sfdx force:package:install --noprompt --targetusername $org_to_install_alias --wait 20 --package $subscriber_package_version_id --upgradetype Delete --loglevel trace --json --apexcompile package  )
 
 Write-Host $install_package_result_json
 $install_package_result = $install_package_result_json| ConvertFrom-Json
