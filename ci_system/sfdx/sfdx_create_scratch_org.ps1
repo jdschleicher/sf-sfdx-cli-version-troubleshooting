432752a59190064301925405b6f61568316bd69f
jdschleicher <jdschleicher@gmail.com>
2023-09-24 17:52:09 -0400

    preparng tmp log artifacts

--- ci_system/sfdx/sfdx_create_scratch_org.ps1
+++ ci_system/sfdx/sfdx_create_scratch_org.ps1
@@ -3,6 +3,6 @@ param( $devhub_alias, $scratch_org_alias)
 Write-Host "CREATING SCRATCH ORG devhub_alias: $devhub_alias"
 Write-Host "CREATING SCRATCH ORG scratch_org_alias: $scratch_org_alias"
 
-$create_scratch_result_json = $(sfdx force:org:create --targetdevhubusername $devhub_alias --definitionfile config/project-scratch-def.json --wait 15 --durationdays 1 -a $scratch_org_alias --json )
+$create_scratch_result_json = $(sfdx force:org:create --targetdevhubusername $devhub_alias --definitionfile config/project-scratch-def.json --wait 15 --durationdays 1 -a $scratch_org_alias --json  --verbose --dev-debug --preserve)
 Write-Host "sfdx create scratch: $create_scratch_result_json"
 
