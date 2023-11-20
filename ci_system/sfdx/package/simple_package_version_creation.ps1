117b9d0224a3c8ac653e570a2bcf1da7406effa8
jdschleicher <jdschleicher@gmail.com>
2023-11-20 11:15:01 -0500

    TROUBLESHOTOTING

--- ci_system/sfdx/package/simple_package_version_creation.ps1
+++ ci_system/sfdx/package/simple_package_version_creation.ps1
@@ -7,7 +7,42 @@ param(
     $package_name
 )
 
-Import-Module -DisableNameChecking "$PSScriptRoot/../package/package_version_service.psm1"
+
+function create_full_validation_package_version_and_get_result {
+    param($package_detail, $devhub_alias)
+    $package_creation_result_json = create_full_validation_package_version_by_devhub_and_package_details -package_detail $package_detail -devhub_alias $devhub_alias
+    Write-Host "package creation result: $package_creation_result_json"
+    $package_version_creation_result = $package_creation_result_json | ConvertFrom-Json
+    $package_version_creation_result
+}
+
+function create_full_validation_package_version_by_devhub_and_package_details {
+    param($package_detail, $devhub_alias)
+
+    Write-Host "package tag: $($package_detail.package_tag)"
+    Write-Host "sf package version create --target-dev-hub $devhub_alias --package "$($package_detail.package_name)" --tag "$($package_detail.package_tag)" --code-coverage --installation-key-bypass --wait 120 --json"
+    $package_creation_result_json = $( sf package version create --target-dev-hub $devhub_alias --package "$($package_detail.package_name)" --tag "$($package_detail.package_tag)" --code-coverage --installation-key-bypass --wait 120 --json) 
+
+    $package_creation_result_json
+    
+}
+
+function create_skip_validation_package_version_and_get_result {
+    param($package_detail, $devhub_alias)
+    $package_creation_result_json = create_skipvalidation_validation_package_version_by_devhub_and_package_details -package_detail $package_detail -devhub_alias $package_version_creation_details.devhub_alias
+    $package_version_creation_result = $package_creation_result_json | ConvertFrom-Json
+    $package_version_creation_result
+}
+
+function create_skipvalidation_validation_package_version_by_devhub_and_package_details {
+    param($package_detail, $devhub_alias)
+    
+    Write-Host "sf package version create --target-dev-hub $devhub_alias --package "$($package_detail.package_name)" --tag "$($package_detail.package_tag)" --skip-validation --installation-key-bypass --wait 10 --json --verbose"
+    $package_creation_result_json = $( sf package version create --target-dev-hub $devhub_alias --package "$($package_detail.package_name)" --tag "$($package_detail.package_tag)" --skip-validation --installation-key-bypass --wait 10 --json --verbose )  
+    
+    $package_creation_result_json
+
+}
 
 $package_version_creation_result = $null
 
